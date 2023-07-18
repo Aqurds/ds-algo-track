91378d3fd82314d5a4e2a986e670fe589d46bb38
aqurds <omarf1320@gmail.com>
2023-07-19 00:22:35 +0600

    Add dist

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import { useState } from 'react'
-import TodoApp from '@/components/TodoApp';
+import TodoApp from './components/TodoApp';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
